fef234101bd8e4d8dd88285926d5a6f53ba34044
khoadl-zs <c.khoa.dang.le@zonarsystems.com>
2021-02-03 17:16:37 +0700

    Finish!!!!

--- CountDownApp/app/src/main/java/com/example/countdownapp/MainActivity.kt
+++ CountDownApp/app/src/main/java/com/example/countdownapp/MainActivity.kt
@@ -4,16 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.opengl.Visibility
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.provider.Settings
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.countdownapp.MyTimer.Companion.EXTRA_KEY_DATA
 import com.example.countdownapp.MyTimer.Companion.INTENT_ACTION
 import com.example.countdownapp.databinding.ActivityMainBinding
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var myReceiver: BroadcastReceiver
+    private lateinit var myReceiver: MyBroadcast
+    private lateinit var myTimer: MyTimer
+    private var shouldStartNewTimer = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +34,169 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        var myTimer = MyTimer(applicationContext, 50000)
-        myTimer.start()
+        myTimer = MyTimer(applicationContext, 0)
+
+        binding.buttonStartPause.setOnClickListener(View.OnClickListener {
+
+            val hour = binding.edtHour.text.toString().toSafeInt()
+            val minute = binding.edtMinute.text.toString().toSafeInt()
+            val second = binding.edtSecond.text.toString().toSafeInt()
+
+            if (validateInput(hour, minute, second)) {
+                myTimer.run {
+
+                    if (shouldStartNewTimer) {
+                        setElapseTime(getElapseTime(hour, minute, second)).also {
+                            shouldStartNewTimer = false
+                            showElapseTime(true)
+                        }
+                    }
+
+                    if (timerIsRunning()) {
+                        pauseTimer()
+                        binding.buttonStartPause.text = resources.getString(R.string.resume)
+                    } else {
+                        startTimer()
+                        binding.buttonStartPause.text = resources.getString(R.string.pause)
+                    }
+                }
+            }
+        })
+
+        binding.buttonStop.setOnClickListener(View.OnClickListener {
+            shouldStartNewTimer = true
+            myTimer.stopTimer()
+            binding.buttonStartPause.text = resources.getString(R.string.start)
+            resetUI()
+        })
 
     }
 
     override fun onStart() {
         super.onStart()
+
+        val filter = IntentFilter().apply {
+            addAction(INTENT_ACTION)
+        }
+
+        myTimer.run {
+            if (!timerIsRunning() && elapsedTime() == 0L) {
+                resetUI()
+            }
+        }
+
+        myReceiver = MyBroadcast()
+        registerReceiver(myReceiver, filter)
     }
 
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(myReceiver)
+    }
+
+    private fun getElapseTime(hour: Int, minute: Int, second: Int): Long {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            add(Calendar.HOUR, hour)
+            add(Calendar.MINUTE, minute)
+            add(Calendar.SECOND, second)
+        }
+
+        val elapseTime = calendar.timeInMillis - System.currentTimeMillis()
+
+        binding.timerProgress.apply {
+            max = elapseTime.toProgress()
+            progress = elapseTime.toProgress()
+        }
+
+        return elapseTime
+    }
+
+    private fun showElapseTime(shoudShow: Boolean) {
+        if (shoudShow) {
+            binding.textElapsedTime.visibility = View.VISIBLE
+            binding.setTimeLayout.visibility = View.INVISIBLE
+        } else {
+            binding.textElapsedTime.visibility = View.INVISIBLE
+            binding.setTimeLayout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun resetUI() {
+        showElapseTime(false)
+        shouldStartNewTimer = true
+        binding.edtHour.text.clear()
+        binding.edtMinute.text.clear()
+        binding.edtSecond.text.clear()
+        binding.buttonStartPause.text = resources.getString(R.string.start)
+        binding.timerProgress.apply {
+            max = 100
+            progress = 100
+        }
+    }
+
+    private fun String.toSafeInt(): Int {
+        return takeIf { it.isNotEmpty() }?.toInt() ?: 0
+    }
+
+    private fun validateInput(hour: Int, minute: Int, second: Int): Boolean {
+        if (hour == 0 && minute == 0 && second == 0 ) {
+            Toast.makeText(this@MainActivity, resources.getString(R.string.invalid_input), Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (minute > 59 || second > 59) {
+            Toast.makeText(this@MainActivity, resources.getString(R.string.invalid_input), Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun Long.toProgress(): Int {
+        return (this/1000).toInt()
+    }
+
+
+    inner class MyBroadcast: BroadcastReceiver() {
+
+        override fun onReceive(p0: Context?, p1: Intent?) {
+
+            p1?.let {
+                val data = it.getLongExtra(EXTRA_KEY_DATA, 0L)
+
+                if (data != 0L) {
+                    binding.textElapsedTime.text = hmsTimeFormatter(data)
+                    binding.timerProgress.apply {
+                        progress = data.toProgress()
+                    }
+                } else {
+                    resetUI()
+                }
+            }
+        }
+
+        /**
+         * method to convert millisecond to time format
+         *
+         * @param milliSeconds
+         * @return HH:mm:ss time formatted string
+         */
+        private fun hmsTimeFormatter(milliSeconds: Long): String? {
+            return String.format(
+                "%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(milliSeconds),
+                TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(
+                    TimeUnit.MILLISECONDS.toHours(
+                        milliSeconds
+                    )
+                ),
+                TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(
+                    TimeUnit.MILLISECONDS.toMinutes(
+                        milliSeconds
+                    )
+                )
+            )
+        }
+    }
 }
--- CountDownApp/app/src/main/java/com/example/countdownapp/MyTimer.kt
+++ CountDownApp/app/src/main/java/com/example/countdownapp/MyTimer.kt
@@ -3,30 +3,91 @@ package com.example.countdownapp
 import android.content.Context
 import android.content.Intent
 import android.os.CountDownTimer
+import android.provider.Settings
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.lang.ref.WeakReference
 
-class MyTimer(context: Context, millisInFuture: Long): CountDownTimer(millisInFuture, COUNT_DOWN_INTERNAL) {
+class MyTimer(context: Context, millisInFuture: Long) {
     private var context: WeakReference<Context> = WeakReference(context)
+    private var millisInFuture = millisInFuture
+    private var isRunning = false
+    private lateinit var internalTimer : InternalTimer
 
-    override fun onTick(millisUntilFinished: Long) {
-        context.get()?.let {context ->
-            val intent = generateIntent(millisUntilFinished = millisUntilFinished, isCompleted = false)
-            context.sendBroadcast(intent)
+    inner class InternalTimer(millisInFuture: Long): CountDownTimer(millisInFuture, COUNT_DOWN_INTERNAL) {
+        var remainingTime = 0L
+        override fun onTick(millisUntilFinished: Long) {
+            remainingTime = millisUntilFinished
+            context.get()?.let {context ->
+                val intent = generateIntent(millisUntilFinished = millisUntilFinished)
+                context.sendBroadcast(intent)
+            }
+        }
+
+        override fun onFinish() {
+            context.get()?.let {context ->
+                isRunning = false
+                millisInFuture = 0
+                val intent = generateIntent(millisUntilFinished = 0)
+                context.sendBroadcast(intent)
+                sendCompleteNotification(context)
+            }
+        }
+
+        private fun sendCompleteNotification(context: Context) {
+            val builder = NotificationCompat.Builder(context, MyApplication.NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(context.resources.getString(R.string.timer_finished_notification_title))
+                .setContentText(context.resources.getString(R.string.timer_finished_notification_msg))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .build()
+
+            NotificationManagerCompat.from(context).apply {
+                this.notify(111, builder)
+            }
         }
     }
 
-    override fun onFinish() {
-        context.get()?.let {context ->
-            val intent = generateIntent(millisUntilFinished = 0, isCompleted = true)
-            context.sendBroadcast(intent)
+    fun startTimer() {
+        if (millisInFuture == 0L) {
+            return
+        }
+
+        if (!isRunning && internalTimer.remainingTime > 0) {
+            internalTimer = InternalTimer(internalTimer.remainingTime)
+        }
+        internalTimer.start()
+        isRunning = true
+    }
+
+    fun pauseTimer() {
+        if (isRunning) {
+            internalTimer.cancel()
+            isRunning = false
         }
     }
 
-    private fun generateIntent(millisUntilFinished: Long, isCompleted: Boolean): Intent {
+    fun stopTimer() {
+        isRunning = false
+        internalTimer.remainingTime = 0
+        internalTimer.cancel()
+        internalTimer = InternalTimer(millisInFuture)
+    }
+
+    fun timerIsRunning() = isRunning
+
+    fun elapsedTime() = millisInFuture
+
+    fun setElapseTime(time: Long) {
+        millisInFuture = time
+        internalTimer = InternalTimer(millisInFuture)
+    }
+
+    private fun generateIntent(millisUntilFinished: Long): Intent {
         return Intent().also {
             it.action = INTENT_ACTION
             it.putExtra(EXTRA_KEY_DATA, millisUntilFinished)
-            it.putExtra(EXTRA_KEY_COMPLETED, millisUntilFinished)
         }
     }
 
@@ -34,6 +95,5 @@ class MyTimer(context: Context, millisInFuture: Long): CountDownTimer(millisInFu
         val COUNT_DOWN_INTERNAL = 1000L
         val INTENT_ACTION = "com.example.countdownapp.customaction"
         val EXTRA_KEY_DATA = "data"
-        val EXTRA_KEY_COMPLETED = "completed"
     }
 }
